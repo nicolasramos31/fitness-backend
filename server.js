fa9323acbec7916af44c3f739cb860f539389649
nicolasramos31 <nicomv06@gmail.com>
2026-05-14 00:39:19 -0300

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,9 @@ connectDB();
 const app = express();
 
 // ── Middlewares ──
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ limit: "20mb", extended: true }));
 
